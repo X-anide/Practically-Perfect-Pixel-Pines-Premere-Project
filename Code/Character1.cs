be96bdab36b91df72d70ef4351d48d32f7a5cf27
X-anide <x.anide.alot@gmail.com>
2025-01-15 17:33:04 -0600

    Getting used to working with scenes, sprites, and static bodies. This is a very early version of things as I mess around with things to do. Felt cute. Might delete (a lot of this) later as I figure out what I'm actually doing.

---
+++ Code/Character1.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Character1 : CharacterBody2D
+{
+    public override void _PhysicsProcess(double delta) {
+        var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        this.Velocity = direction  * 200;
+        MoveAndSlide();
+    }
+}
